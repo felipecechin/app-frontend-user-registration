413c5348fb6c81d0f3ebe2f675f212dd4b48dc30
Felipe Cechin <ficechin@gmail.com>
2022-12-04 17:37:27 -0300

    signin page

---
+++ pages/user/signin.tsx
@@ -0,0 +1,25 @@
+import SigninForm from '@/components/system/signin/SigninForm'
+
+export default function Signin(): JSX.Element {
+    return (
+        <main>
+            <div className='flex min-h-screen h-fit py-4 bg-cyan-800 items-center'>
+                <div className='container px-4 mx-auto'>
+                    <div className='bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto py-6 px-4 sm:px-10'>
+                        <div className='mx-auto'>
+                            <div className='text-center mb-8'>
+                                <h2 className='flex flex-row justify-center text-cyan-800 font-bold text-4xl'>
+                                    Olá visitante!
+                                </h2>
+                            </div>
+                            <SigninForm />
+                            <p className='mt-4 text-center text-sm text-gray-800 font-semibold'>
+                                Não tem uma conta? Cadastre-se aqui
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
---
+++ src/components/system/signin/SigninForm/index.tsx
@@ -0,0 +1,65 @@
+import * as yup from 'yup'
+
+import { useCallback, useMemo } from 'react'
+
+import { FaSignInAlt } from 'react-icons/fa'
+import InputGroup from '@/components/shared/form/InputGroup'
+import { SubmitHandler } from 'react-hook-form'
+import { useAuth } from '@/contexts/AuthContext'
+import { useFormWithSchema } from '@/hooks/useFormWithSchema'
+import { yupMessages } from '@/utils/yupMessages'
+
+export default function SigninForm(): JSX.Element {
+    const { signin } = useAuth()
+
+    const loginSchema = useMemo(() => {
+        return yup.object({
+            email: yup
+                .string()
+                .email(yupMessages.email)
+                .required(yupMessages.required),
+            password: yup.string().required(yupMessages.required),
+        })
+    }, [])
+
+    const {
+        handleSubmit,
+        formState: { errors },
+        register,
+    } = useFormWithSchema(loginSchema)
+
+    const handleFormSubmit = useCallback<
+        SubmitHandler<yup.Asserts<typeof loginSchema>>
+    >(
+        async (data) => {
+            console.log(data)
+        },
+        [signin]
+    )
+
+    return (
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <div className='space-y-6'>
+                <InputGroup
+                    error={errors.email?.message as string}
+                    label='E-mail'
+                    name='email'
+                    placeholder='Digite seu e-mail'
+                    register={register}
+                    type='email'
+                />
+                <InputGroup
+                    error={errors.password?.message as string}
+                    label='Senha'
+                    name='password'
+                    placeholder='Digite sua senha'
+                    register={register}
+                    type='password'
+                />
+            </div>
+            <button className='mt-10 flex items-center justify-center w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-cyan-800 hover:bg-cyan-900 border-3 border-cyan-900 shadow rounded transition duration-200'>
+                <FaSignInAlt className='w-6 h-6 mr-2' /> Entrar
+            </button>
+        </form>
+    )
+}
