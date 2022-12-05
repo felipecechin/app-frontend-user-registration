105349e4217e916abd2e38b40979163805450afe
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-05 08:57:03 -0300

    updated SigninForm component

--- src/components/system/user/signin/SigninForm/index.tsx
+++ src/components/system/user/signin/SigninForm/index.tsx
@@ -40,22 +40,28 @@ export default function SigninForm(): JSX.Element {
     return (
         <form onSubmit={handleSubmit(handleFormSubmit)}>
             <div className='space-y-6'>
-                <InputGroup
-                    error={errors.email?.message as string}
-                    label='E-mail'
-                    name='email'
-                    placeholder='Digite seu e-mail'
-                    register={register}
-                    type='email'
-                />
-                <InputGroup
-                    error={errors.password?.message as string}
-                    label='Senha'
-                    name='password'
-                    placeholder='Digite sua senha'
-                    register={register}
-                    type='password'
-                />
+                <div>
+                    <InputGroup
+                        error={errors.email?.message as string}
+                        label='E-mail'
+                        name='email'
+                        placeholder='Digite seu e-mail'
+                        register={register}
+                        style='big'
+                        type='email'
+                    />
+                </div>
+                <div>
+                    <InputGroup
+                        error={errors.password?.message as string}
+                        label='Senha'
+                        name='password'
+                        placeholder='Digite sua senha'
+                        register={register}
+                        style='big'
+                        type='password'
+                    />
+                </div>
             </div>
             <button className='mt-10 flex items-center justify-center w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-cyan-800 hover:bg-cyan-900 border-3 border-cyan-900 shadow rounded transition duration-200'>
                 <FaSignInAlt className='w-6 h-6 mr-2' /> Entrar
