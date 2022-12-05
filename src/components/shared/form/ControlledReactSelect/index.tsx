f80611b8774a56c131fd9fe4a5e1cbb23ea850b5
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-05 15:55:12 -0300

    added react-select

---
+++ src/components/shared/form/ControlledReactSelect/index.tsx
@@ -0,0 +1,10 @@
+import Select from 'react-select'
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+]
+
+export default function ControlledReactSelect(): JSX.Element {
+    return <Select options={options} />
+}
