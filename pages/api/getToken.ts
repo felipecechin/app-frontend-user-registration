d8177afa615d25978e685c0e93f62e181cdc2983
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-06 09:52:14 -0300

    updated getToken route

--- pages/api/getToken.ts
+++ pages/api/getToken.ts
@@ -2,10 +2,25 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { getToken } from '@/utils/cookies'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<string>): Promise<void> {
+interface IResponseSuccess {
+    data: string
+}
+
+interface IResponseError {
+    message: string
+    status: number
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<IResponseSuccess | IResponseError>
+): Promise<void> {
     const token = getToken(req)
     if (token) {
-        return res.json(token)
+        return res.json({ data: token })
     }
-    return res.status(401).json('Unauthorized')
+    return res.status(401).json({
+        message: 'Unauthorized',
+        status: 401,
+    })
 }
