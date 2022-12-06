028497176fda53fb127c1fe22479b79462d3bd85
Felipe Cechin <ficechin@gmail.com>
2022-12-05 23:21:55 -0300

    initial page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,23 @@
+import Footer from '@/components/shared/template/Footer'
+import Header from '@/components/shared/template/Header'
+import MainContent from '@/components/shared/template/MainContent'
+
+// export const getServerSideProps = withSSRAuth(async ({ token }) => {
+//     return {
+//         props: {},
+//     }
+// })
+
 export default function Home(): JSX.Element {
-    return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    return (
+        <>
+            <Header />
+            <MainContent>
+                <div className='flex flex-col'>
+                    <div className='bg-white rounded-lg shadow-lg px-4 py-4 flex flex-col'></div>
+                </div>
+            </MainContent>
+            <Footer />
+        </>
+    )
 }
