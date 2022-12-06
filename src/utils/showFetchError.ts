63be02639aead43f9db6f8b44d0a709f53a93a1c
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-06 13:31:57 -0300

    showFetchError function

---
+++ src/utils/showFetchError.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { reactSwal, sweetAlertOptions } from './sweetAlert'
+
+import FetchError from './FetchError'
+
+const apiErrorMessages: Record<string, string> = {
+    'Email already in use': 'E-mail já está sendo usado. Por favor, informe outro.',
+    'Individual number already in use': 'CPF já está sendo usado. Por favor, informe outro.',
+    'Worker number already in use': 'PIS já está sendo usado. Por favor, informe outro.',
+}
+
+function showFetchError(error: any): void {
+    let message: string
+    if (error instanceof FetchError && error.data.message) {
+        const messageKey = error.data.message as string
+        message = apiErrorMessages[messageKey] || 'Ocorreu um erro desconhecido'
+    } else {
+        message = 'Ocorreu um erro desconhecido'
+    }
+    reactSwal.fire({
+        title: 'Oops!',
+        icon: 'error',
+        text: message,
+        confirmButtonColor: sweetAlertOptions.confirmButtonColor,
+    })
+}
+
+export default showFetchError
