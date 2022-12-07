17b2faf773e4224ffdf682ccaefca87242b829a9
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-07 17:57:02 -0300

    updated setupTests.ts

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -6,9 +6,9 @@ jest.mock('next/dynamic', () => ({
     default: (...props: any[]) => {
         const dynamicModule = jest.requireActual('next/dynamic')
         const dynamicActualComp = dynamicModule.default
-        const RequiredComponent = dynamicActualComp(props[0])
-        RequiredComponent.preload ? RequiredComponent.preload() : RequiredComponent.render.preload()
-        return RequiredComponent
+        const requiredComponent = dynamicActualComp(props[0])
+        requiredComponent.preload ? requiredComponent.preload() : requiredComponent.render.preload()
+        return requiredComponent
     },
 }))
 
