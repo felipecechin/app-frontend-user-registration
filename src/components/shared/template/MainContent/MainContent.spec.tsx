d89b41ba8a240ad0174d80a230b2e814682e3c6a
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-07 15:14:39 -0300

    first unit tests

---
+++ src/components/shared/template/MainContent/MainContent.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+
+import MainContent from '.'
+
+describe('MainContent component', () => {
+    it('should render correctly', () => {
+        render(<MainContent>Content test</MainContent>)
+
+        expect(screen.getByText('Content test')).toBeInTheDocument()
+    })
+
+    it('should has main tag', () => {
+        const { container } = render(
+            <MainContent>
+                <h1>Test</h1>
+            </MainContent>
+        )
+
+        const main = container.querySelector('main')
+        expect(main).toBeInTheDocument()
+    })
+})
