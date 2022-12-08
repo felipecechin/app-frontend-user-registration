6630070ccddd46eef7df31a157c0938cb13dd01a
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-08 16:40:42 -0300

    updated Home page test

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as cookiesFunctions from '@/utils/cookies'
+
+import Home, { getServerSideProps } from '../../../pages'
 import { render, screen } from '@testing-library/react'
 
-import Home from '../../../pages'
+import fetcher from '@/utils/fetcher'
 
 const profileMock = {
     email: 'ficechin@hotmail.com',
@@ -17,11 +21,47 @@ const profileMock = {
     },
 }
 
+jest.mock('@/utils/fetcher')
+jest.mock('@/utils/cookies')
+
 describe('Home page', () => {
     it('should render correctly', () => {
         render(<Home profile={profileMock} />)
 
         expect(screen.getByText('Olá, ' + profileMock.name + '!')).toBeInTheDocument()
         expect(screen.getByText('Editar dados')).toBeInTheDocument()
+    })
+
+    it('should redirect if token does not exist', async () => {
+        const getTokenMock = jest.spyOn(cookiesFunctions, 'getToken')
+        getTokenMock.mockReturnValue('')
+
+        const response = await getServerSideProps({} as any)
+        expect(response).toEqual(
+            expect.objectContaining({
+                redirect: expect.objectContaining({
+                    destination: '/users/signin',
+                }),
+            })
+        )
+    })
+
+    it('should load initial page if token exists', async () => {
+        const getTokenMock = jest.spyOn(cookiesFunctions, 'getToken')
+        getTokenMock.mockReturnValue('token')
+
+        const fetcherMock = jest.mocked(fetcher)
+        fetcherMock.mockResolvedValue({
+            profile: profileMock,
+        })
+
+        const response = await getServerSideProps({} as any)
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    profile: profileMock,
+                },
+            })
+        )
     })
 })
