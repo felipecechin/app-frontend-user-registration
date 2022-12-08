271bb2464af38aff9b0d23641b219925ff1d046e
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-08 15:58:17 -0300

    formatted signup page code

--- pages/users/signup.tsx
+++ pages/users/signup.tsx
@@ -5,9 +5,7 @@ export default function Signup(): JSX.Element {
         <main>
             <div className='flex min-h-screen h-fit py-4 bg-cyan-800 items-center'>
                 <div className='w-full max-w-5xl px-4 mx-auto'>
-                    <h2 className='text-center text-white text-3xl font-bold my-4'>
-                        Cadastro
-                    </h2>
+                    <h2 className='text-center text-white text-3xl font-bold my-4'>Cadastro</h2>
                     <div className='bg-white rounded-lg shadow-lg p-6'>
                         <SignupForm />
                     </div>
